e55871cad845b926a520e6900f964aec5066393f
jemelsalby <jemelsalby3@gmail.com>
2022-10-15 14:53:08 +0530

    sa

---
+++ src/test/java/com/nestdigital/studentApp/StudentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestdigital.studentApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
